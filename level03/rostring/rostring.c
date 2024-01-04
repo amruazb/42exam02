7459bf1d5d9f16b90be9fa881f5eb7c3035dbaeb
Aamir Zuhail Basheer <132408385+amruazb@users.noreply.github.com>
2024-01-04 07:34:16 +0400

    rostring added

---
+++ level03/rostring/rostring.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int is_space(char c)
+{
+	return (c == ' ' || c == '\t');
+}
+
+void rostring(char *str)
+{
+	while (is_space(*str))
+		str++;
+	if(*str && !is_space(*str))
+		write(1, str++, 1);
+	while(*str)
+	{
+		if (is_space(*str))
+		{
+			++str;
+			continue;
+		}
+		if(is_space(*(str - 1)))
+			write(1, " ", 1);
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int main(int ac,char **av)
+{
+	if(ac == 2)
+		rostring(av[1]);
+	write(1, "\n", 1);
+}
+
