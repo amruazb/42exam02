cf9a3bc98b308ae5d1ac08eff395f4df999ddbd6
Aamir Zuhail Basheer <132408385+amruazb@users.noreply.github.com>
2024-01-16 16:12:24 +0400

    hidenp

---
+++ level02/hidenp/hidenp.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int hidenp(char *s1, char *s2)
+{
+	while(*s1 && *s2)
+	{
+		if(*s1 == *s2)
+			s1++;
+		s2++;
+	}
+	return !*s1;
+}
+
+int main(int ac, char **av)
+{
+	if(ac != 3)
+	{
+		write(1, "\n", 1);
+		return 0;
+	}
+	int i = hidenp(av[1], av[2]);
+	if (hidenp)
+		write(1, "1\n", 1);
+	else
+		write(1, "0\n", 1);
+}
