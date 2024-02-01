09713fb7a54cf3b81a7f21a82a2a97cf48428751
Aamir Zuhail Basheer <132408385+amruazb@users.noreply.github.com>
2024-02-01 09:19:35 +0400

    rostringfixed

--- level03/rostring/rostring 2.c
+++ level03/rostring/rostring 2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 void	rostring(char *s)
@@ -6,23 +7,24 @@ void	rostring(char *s)
 	int i = 0;
 	int j = 0;
 	int k = 0;
-	char ps1[100];
+	char *p = malloc(1024);
 	while(*s && *s == ' ')
 		s++;
 	while(*s && *s != ' ')
-		ps1[i++]= *(s)++;
+		p[i++]= *(s)++;
 	while(*s)
 	{
 		if(*s == ' ' || *s == '\t')
 		{
 			s++;
-			if(*s != '\0' && *s != ' ' && j == 42)
+			if(*s != '\0' && *s != ' ')
 			{
-				write(1, " ", 1);;
+				if(j == 42)
+					write(1, " ", 1);
+				else 
+					j = 42;
 			}
-			else 
-				j = 42;
-		}
+		}	
 		else
 		{
 			write(1, s++,1);
@@ -32,8 +34,8 @@ void	rostring(char *s)
 	i = 0;
 	if(k == 1)
 		write(1, " ", 1);
-	while(ps1[i])
-		write(1, &ps1[i++], 1);
+	while(*p)
+		write(1, p++, 1);
 }
 
 int main(int ac, char **av)
