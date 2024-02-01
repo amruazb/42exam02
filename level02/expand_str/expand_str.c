b27539a21a3a80219a0a43e99659a2c16968321b
Aamir Zuhail Basheer <132408385+amruazb@users.noreply.github.com>
2024-02-01 12:02:00 +0400

    expandstr

---
+++ level02/expand_str/expand_str.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+void	epur(char *s)
+{
+	int i = 0;
+	int lastspace = 0;
+	while(*s && *s == ' ')
+		s++;
+	while(*s &&  *s == ' ')
+		s--;
+	while(*s && *s)
+	{
+		if(*s && *s  ==  ' ')
+		{
+			while(*s && *s == ' ')
+				s++;;
+			s--;
+			lastspace = 1;
+		}
+		else
+		{
+			if(lastspace)
+			{
+				write(1, "   ", 3);
+				lastspace = 0;
+			}
+			write(1, s, 1);
+		}
+		s++;
+	}
+}
+
+int main(int ac, char **av)
+{
+	if(ac == 2)
+		epur(av[1]);
+	write(1, "\n",1);
+}
