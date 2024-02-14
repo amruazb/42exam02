1910086512d84d4fa9a3e47b547de3d1bbfdb5bf
Aamir Zuhail Basheer <132408385+amruazb@users.noreply.github.com>
2024-02-14 20:57:30 +0400

    Update wd_match.c

--- level01/wd_match/wd_match.c
+++ level01/wd_match/wd_match.c
@@ -1,26 +1,24 @@
 #include <unistd.h>
 
-void wd_match(char *s1, char *s2)
+void wdmatch(char *s1, char *s2)
 {
-	int i = 0;
-	char *ps1 = s1;
-	while(s1[i])
-		i++;
-	int j = 0;
-	while (*s2 && j < i)
-	{
-		if(*s2 == ps1[j])
-			j++;
-		s2++;
-	}
-	if(j == i)
-		write(1, ps1, i);
+    char *p1 = s1;
+    while(*s2 && *s1)
+    {
+        if(*s1 == *s2)
+            s1++;
+        s2++;
+    }
+    if(!*s1)
+        while(*p1)
+            write(1,p1++,1);
 }
-int main(int ac, char **av)
+
+int main(int ac,char **av)
 {
-	if(ac == 3)
-		wd_match(av[1], av[2]);
-	write(1, "\n", 1);
-}
+    if(ac == 3)
+        wdmatch(av[1],av[2]);
+    write(1, "\n",1);
+}}
 
 
